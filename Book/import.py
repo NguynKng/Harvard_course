6f29ae436514209f65f92185474daee9141515c0
NguynKng <harrybin2109@gmail.com>
2023-12-20 11:40:41 +0700

    test

--- Book/import.py
+++ Book/import.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os
 import csv
-
+#test
 engine = create_engine(os.getenv("DATABASE_URL"))
 
 db = scoped_session(sessionmaker(bind=engine)) 
