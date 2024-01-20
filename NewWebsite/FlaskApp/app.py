f5832c33ed5f47fe34b83a4a776721b17c11f6d6
NguynKng <harrybin2109@gmail.com>
2024-01-20 12:04:05 +0700

    Flask app update

--- NewWebsite/FlaskApp/app.py
+++ NewWebsite/FlaskApp/app.py
@@ -1,24 +1,36 @@
 from flask import Flask, render_template, request, json
-from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = "zzharrybin219"
+app.config['MYSQL_DB'] = "demo"
+app.config['MYSQL_DATABASE_PORT'] = 3306
+mysql.init_app(app)
 
 @app.route("/")
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/showSignUp')
 def showSignUp():
     return render_template("signup.html")
 
-@app.route("/signup",methods=['POST'])
+@app.route("/signup",methods=["POST"])
 def signup():
-    _name = request.form('name')
-    _password = request.form('password')
-    _email = request.form('email')
-    if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
-    
+    if request.method == "POST":
+        _name = request.form('name')
+        _password = request.form('password')
+        _email = request.form('email')
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO customer (user_name,user_username,user_password) VALUES (%s,%s,%s)",(_name,_password,_email))
+        mysql.connection.commit()
+        cursor.close()
+
+        return "Bạn đã đăng ký thành công"
+    return json.dumps({'html':'<span>All fields good !!</span>'})
 if __name__ == "__main":
     app.run()
